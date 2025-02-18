31ce9381b1438cfd3379af79a7f8391fadbb6b27
Mazz-Ather <mazzather@gmail.com>
2025-02-18 05:41:32 +0500

    Initial commit

--- src/components/Home/Marquee/Marquee.tsx
+++ src/components/Home/Marquee/Marquee.tsx
@@ -1,22 +1,10 @@
 "use client";
 import Image from "next/image";
-import { useState } from "react";
 
 export default function Marquee() {
-  const [isHovered, setIsHovered] = useState(false);
-
   return (
-    <div
-      className="overflow-hidden relative w-full py-4"
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-    >
-      <div
-        className={`flex items-center w-full gap-8 ${
-          isHovered ? "animate-none" : "animate-marquee"
-        }`}
-      >
-        {/* Original images (no duplication) */}
+    <div className="overflow-hidden relative w-full py-4">
+      <div className="flex items-center w-full gap-8 animate-marquee">
         {[
           "/images/tes1.png",
           "/images/tes2.png",
@@ -26,14 +14,13 @@ export default function Marquee() {
             key={index}
             src={src}
             alt="Brand Logo"
-            width={150} // Adjust size to fit full width naturally
+            width={150}
             height={50}
             className="object-contain"
           />
         ))}
       </div>
 
-      {/* Tailwind CSS for Marquee Animation */}
       <style jsx>{`
         @keyframes marquee {
           from {
